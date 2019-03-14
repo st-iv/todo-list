17026a01dda26fab750a5cf388c50b8289108692
st-iv <stepan.ivanov93@gmail.com>
2019-03-14 17:19:18 +0800

    #add onLabelClick function

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,18 +2,24 @@ import React, {Component} from 'react'
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
+
+    onLabelClick() {
+        console.log(`Done:${this.props.label}`)
+    }
+
     render() {
         const {label, important = false} = this.props;
 
         const style = {
-            color: important ? 'tomato' : 'black'
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important? 'bold' : 'normal'
         }
 
         return (
-            <div className="todo-list-item" style={style}>
-            <span>
-                {label}
-            </span>
+            <div className="todo-list-item">
+                <span className="todo-list-item-label" style={style} onClick={ this.onLabelClick.bind(this) }>
+                    {label}
+                </span>
                 <button type="button" className="btn btn-outline-success btn-sm float-right">
                     <i className="fa fa-exclamation"/>
                 </button>
