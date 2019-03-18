51a5720a22d010826605c8872024bc730cad61c6
st-iv <stepan.ivanov93@gmail.com>
2019-03-18 11:41:22 +0800

    app to class component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './app.css'
 
 import AppHeader from '../app-header/app-header'
@@ -7,24 +7,24 @@ import TodoList from '../todo-list/todo-list'
 import ItemStatusFilter from '../item-status-filter/item-status-filter'
 
 
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink coffee', important: false},
-        {label: 'Build app', important: true},
-        {label: 'Have a lunch', important: false},
-    ]
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo="3" done="2" />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+export default class App extends Component {
+    state = {
+        todoData: [
+            {label: 'Drink coffee', important: false},
+            {label: 'Build app', important: true},
+            {label: 'Have a lunch', important: false},
+        ]
+    }
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo="3" done="2" />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList todos={this.state.todoData} onDeleted={(key) => console.log('del',key)}/>
             </div>
-            <TodoList todos={todoData} onDeleted={(key) => console.log('del',key)}/>
-        </div>
-    )
+        )
+    }
 }
-
-export default App;
